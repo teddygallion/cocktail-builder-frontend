986cc1a87790504f8454b4a83e5563be5d9e81dc
Huiying Cao <huiyingcao2@gmail.com>
2025-04-07 16:08:48 -0700

    wip: added cocktail details, cocktail list is now protected

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,8 @@ import Landing from './components/Landing/Landing';
 import Dashboard from './components/Dashboard/Dashboard';
 import { UserContext } from './contexts/UserContext';
 import CocktailList from './components/CocktailList/CocktailList';
+import CocktailDetails from './components/CocktailDetails/CocktailDetails.jsx';
 import CocktailsData from "./cocktailsData.js"
-
-
 import * as cocktailService from './services/cocktailService';
 
 
@@ -21,18 +20,18 @@ function App() {
     <>
       <NavBar />
       <Routes>
-        <Route
-          path='/'
-          element={user ? <Dashboard /> : <Landing />} 
-        />
-        <Route
-          path="/sign-up"
-          element={<SignUpForm />}
-        />
-        <Route
-          path="/sign-in"
-          element={<SignInForm />}
-        />
+        <Route path='/' element={user ? <Dashboard /> : <Landing />} />
+        {user ? (
+          <>
+            <Route path='/cocktails' element={<CocktailList />} />
+            <Route path='/cocktails/:cocktailId' element={<CocktailDetails />} />
+          </>
+        ) : (
+          <>
+            <Route path='/sign-up' element={<SignUpForm />} />
+            <Route path='/sign-in' element={<SignInForm />} />
+          </>
+        )}
       </Routes>
     </>
   );
