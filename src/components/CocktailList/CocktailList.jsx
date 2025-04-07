ad6978e9988c4c9879ecc712276051150d21d387
latrisha <l_gomer@yahoo.com>
2025-04-07 15:28:27 -0700

    wip: write cocktailList function

--- src/components/CocktailList/CocktailList.jsx
+++ src/components/CocktailList/CocktailList.jsx
@@ -1,7 +1,42 @@
-const CocktailList = (props) => {
-    return <main>Cocktail List</main>;
-  };
-  
-  export default CocktailList;
-  
+
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { getRandom } from '../../services/cocktailService';
+
+const CocktailList = () => {
+  const [cocktails, setCocktails] = useState([]);
+
+  useEffect(() => {
+    const fetchCocktails = async () => {
+      const promises = Array.from({ length: 5 }, () => getRandom());
+      const results = await Promise.all(promises);
+
+
+      const validResults = results.filter(Boolean);
+      setCocktails(validResults);
+    };
+
+    fetchCocktails();
+  }, []);
+
+  if (cocktails.length === 0) return <p>Loading...</p>;
+
+  return (
+    <main>
+      {cocktails.map((cocktail, index) => (
+        <Link key={index} to={`/cocktails/${cocktail.name}`}>
+          <article>
+            <header>
+              <h2>{cocktail.name}</h2>
+              <p>{cocktail.instructions?.slice(0, 100)}...</p>
+            </header>
+            <img src={cocktail.image} alt={cocktail.name} width="300" className="rounded-img" />
+          </article>
+        </Link>
+      ))}
+    </main>
+  );
+};
+
+export default CocktailList;
 
