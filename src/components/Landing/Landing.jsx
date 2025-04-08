c5a1fdf3255108699765272e72d58fd9e4c623f7
latrisha <l_gomer@yahoo.com>
2025-04-08 13:22:42 -0700

    wip: create NavBar css, style NavBar

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,7 +1,7 @@
 const Landing = () => {
     return (
         <main>
-            <h1>Hello, you are on the landing page for visitors.</h1>
+            <h1>Start Building Amazing Cocktails.</h1>
             <p>Sign up now, or sign in to see your super secret dashboard!</p>
         </main>
     );
