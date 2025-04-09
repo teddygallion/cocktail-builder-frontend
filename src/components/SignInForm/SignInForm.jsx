35ac4673dfa81edf2cd59ef9177665a4a6d4ade1
latrisha <l_gomer@yahoo.com>
2025-04-09 16:54:57 -0700

    wip: add className, styles, placeholder to signin form

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -1,72 +1,78 @@
-import { useState, useContext } from 'react';
-import { useNavigate } from 'react-router';
-import { signIn } from '../../services/authService';
-import { UserContext } from '../../contexts/UserContext';
+import { useState, useContext } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { signIn } from "../../services/authService";
+import { UserContext } from "../../contexts/UserContext";
+import styles from "../Form.module.css";
 
 const SignInForm = () => {
-    const navigate = useNavigate();
-    const { setUser } = useContext(UserContext);
-    const [message, setMessage] = useState('');
-    const [formData, setFormData] = useState({
-        username: '',
-        password: ''
+  const navigate = useNavigate();
+  const { setUser } = useContext(UserContext);
+  const [message, setMessage] = useState("");
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (evt) => {
+    setMessage("");
+    setFormData({
+      ...formData,
+      [evt.target.name]: evt.target.value,
     });
+  };
 
-    const handleChange = (evt) => {
-        setMessage('');
-        setFormData({
-            ...formData,
-            [evt.target.name]: evt.target.value
-        })
-    };
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
+    try {
+      const signedInUser = await signIn(formData);
+      setUser(signedInUser);
+      navigate("/");
+    } catch (error) {
+      setMessage(error.message);
+    }
+  };
 
-    const handleSubmit = async (evt) => {
-        evt.preventDefault();
-        try {
-            const signedInUser = await signIn(formData);
-            setUser(signedInUser);
-            navigate('/');
-        } catch (error) {
-            setMessage(error.message);
-        }
-    };
-    
-    return (
-        <main>
-            <h1>Sign In</h1>
-            <p>{message}</p>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="username">Username:</label>
-                    <input 
-                        type="text"
-                        autoComplete="off"
-                        id="username"
-                        value={formData.username}
-                        name="username"
-                        onChange={handleChange}
-                        required 
-                    />
-                </div>
-                <div>
-                    <label htmlFor="password">Password:</label>
-                    <input 
-                        type="password"
-                        autoComplete="off"
-                        id="password"
-                        value={formData.password}
-                        name="password"
-                        onChange={handleChange}
-                        required 
-                    />
-                </div>
-                <div>
-                    <button>Sign In</button>
-                    <button onClick={() => navigate('/')}>Cancel</button>
-                </div>
-            </form>
-        </main>
-    );
+  return (
+    <div className={styles.formWrapper}>
+      <form className={styles.formContainer} onSubmit={handleSubmit}>
+        <h1>Welcome, Sign In</h1>
+        <p>{message}</p>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            autoComplete="off"
+            id="username"
+            value={formData.username}
+            placeholder="Username"
+            name="username"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            autoComplete="off"
+            placeholder="Password"
+            id="password"
+            value={formData.password}
+            name="password"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className={styles.buttonGroup}>
+          <button>Sign In</button>
+          <button onClick={() => navigate("/")}>Cancel</button>
+        </div>
+        <p className={styles.prompt}>
+          Don’t have an account yet? <Link to="/sign-up">Sign Up.</Link>
+        </p>
+      </form>
+    </div>
+  );
 };
 
 export default SignInForm;
