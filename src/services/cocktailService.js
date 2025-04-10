b55416768db745fad053b873a01e78183abef35f
latrisha <l_gomer@yahoo.com>
2025-04-10 13:07:47 -0700

    wip: update getRandom url in services.js

--- src/services/cocktailService.js
+++ src/services/cocktailService.js
@@ -4,7 +4,7 @@ const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/cocktails`;
 
 const getRandom = async () => {
   try {
-    const res = await fetch(`${BASE_URL}/random`);
+    const res = await fetch(`${BASE_URL}/api/random`);
     return res.json();
   } catch (error) {
     console.error("Error fetching random cocktail:", error);
